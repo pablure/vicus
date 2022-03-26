21f8578e464e7fd13e6ceda03012239f5f692cc7
Pablo Gago <pablure@gmail.com>
2022-03-26 12:55:14 +0100

    empezando vicus

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <nav class="text-right mr-5 text-white font-semibold">
+    <router-link class=""  to="/home">Home</router-link>&nbsp;
+    <router-link to="/about">Landing page</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style>
+body{
+  height: 100vh;
+   /*background-color: aqua; */
+   background: radial-gradient(rgb(188, 16, 240), rgb(145, 7, 187));
+  }
+#app {
+  
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <!--<h1>Home</h1>-->
+  <div id="formulario" class="border-2 w-80 h-60 rounded-lg shadow-2xl mx-auto bg-slate-100">
+    <h1 class="my-4 font-semibold">Entrar en VICUS</h1>
+    <form action="#">
+      <div class="elemento-formulario">
+        <label for="nombre">Nombre</label>
+        <input type="text" id="nombre" name="nombre" value="" class="border-2 rounded-md ml-2" required/>
+      </div>
+
+      <div class="elemento-formulario">
+        <label for="apellidos">Apellidos</label>
+        <input type="text" id="apellidos" name="apellidos" value="" required/>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
